246e52621213e0faee066cec359c8cf23c95f3c1
bencolmer <ben.colmer@adi.do>
2026-08-17 11:54:22 +0100

    WIP added deprecated class aliases for pre-MODX3 class names

--- core/components/mediamanager/bootstrap.php
+++ core/components/mediamanager/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/include/deprecated.php';
 
 $modx->addPackage('Sterc\MediaManager\Model', $namespace['path'] . 'src/', null, 'Sterc\\MediaManager\\');
 
